82ff4e396bfd8428f07e55316806d6071ff2a812
Facundo Benitez <facundob91@gmail.com>
2019-09-19 12:13:02 -0300

    add files

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Selectors
+let comicImage = document.querySelector('.comic-img');
+let comicTitle = document.querySelector('.comic-title');
+let comicNumber = document.querySelector('.comic-num');
+let url = 'https://xkcd.com/info.0.json';
+
+function fetchInfo() {
+	// Create XHR Object
+	let request = new XMLHttpRequest();
+	// Open Method (HTTP Method, url, async)
+	request.open('GET', url, true);
+	// Function when request completes
+	request.onload = function() {
+		if (this.status === 200) {
+			let response = JSON.parse(this.responseText);
+			comicImage.src = response.img;
+			comicImage.alt = response.alt;
+			comicTitle.innerHTML = 'Title: ' + response.title;
+			comicNumber.innerHTML = 'Comic Nº: ' + response.num;
+		} else if (this.status === 404) {
+				console.error('Error 404');
+		}
+	};
+	// Send request
+	request.send();
+};
+
+window.onload = fetchInfo();
